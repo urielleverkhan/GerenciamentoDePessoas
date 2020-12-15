01acf019d2523c7f24dc28494241984541a19848
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2020-12-14 23:22:18 -0300

    -Initial commit

---
+++ src/main/java/com/gerenciamentodepessoas/gerenciamentodepessoas/GerenciamentodepessoasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamentodepessoas.gerenciamentodepessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentodepessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentodepessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciamentodepessoas/gerenciamentodepessoas/GerenciamentodepessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentodepessoas.gerenciamentodepessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentodepessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
